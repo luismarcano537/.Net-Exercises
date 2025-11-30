9abb495a140152cc253e8b98a782f6c9b1fc0fac
Luis Silva <luissilvam537@gmail.com>
2025-11-29 23:16:44 -0300

    Add lambda exercises

---
+++ ExerciseLambda/ExerciseLambda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ExerciseLambda.Models;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the full Path file: ");
+            string Path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader Sr = File.OpenText(Path))
+            {
+                while (!Sr.EndOfStream)
+                {
+                    string[] fields = Sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1]);
+
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(P => P.Price).DefaultIfEmpty(0.0).Average();
+
+            Console.WriteLine("Average Price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(P => P.Price < avg).OrderByDescending(P => P.Name).DefaultIfEmpty().Select(P => P.Name);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ ExerciseLambda02/ExerciseLambda02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using ExerciseLambda02.Entities;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter Salary: ");
+            double LimitSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> Employees = new List<Employee>();
+
+            try
+            {
+                using (StreamReader Sr = File.OpenText(path))
+                {
+                    while (!Sr.EndOfStream)
+                    {
+                        string[] fields = Sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        Employees.Add(new Employee(name, email, salary));
+                    }
+                }
+
+                var emails = Employees.Where(E => E.Salary > LimitSalary).OrderBy(E => E.Name).Select(E => E.Email).ToList();
+
+                var AllSallary = Employees.Where(E => E.Name[0] == 'M').Sum(E => E.Salary);
+
+                Console.WriteLine("\nEmail of people whose salarry is more than 2000.00: ");
+                foreach (var em in emails)
+                {
+                    Console.WriteLine(em);
+                }
+
+                Console.WriteLine("\nSum of salary of peoples whose name starts with 'm': " + AllSallary.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (IOException e) {
+
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+
+        }
+    }
+}
